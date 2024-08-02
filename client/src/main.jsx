6590eae5e475446197a4f59319f768e3b56cc621
Krishnanand U <krishnanand.u@armiasystems.com>
2024-08-02 18:17:01 +0530

    chore: add react-persist for state persistence

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,21 @@
 // src/main.jsx
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import App from './App';
-import {RouterProvider} from "react-router-dom"
-import './index.css'
+import { Provider } from "react-redux";
+import {store} from "./redux/store";
+import {persistor} from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import App from "./App";
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    {/* <Navbar /> */}
-    <App/>
-  </Provider>,
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      {/* <Navbar /> */}
+      <App />
+    </Provider>
+  </PersistGate>
 );
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,13 +1,31 @@
 // src/store.js
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './slices/authSlice';
-import taskReducer from './slices/taskSlice';
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import authReducer from "./slices/authSlice";
+import taskReducer from "./slices/taskSlice";
+//redux presist reducer
+import { persistReducer } from "redux-persist";
+import storage  from "redux-persist/lib/storage";
+import persistStore from "redux-persist/es/persistStore";
 
-const store = configureStore({
-    reducer: {
-        auth: authReducer,
-        tasks: taskReducer
-    }
+const rootReducer = combineReducers({
+  auth: authReducer,
+  tasks: taskReducer,
 });
 
-export default store;
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false
+    }),
+});
+
+export const persistor = persistStore(store);
